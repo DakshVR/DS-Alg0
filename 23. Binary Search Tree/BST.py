0dff837d8c16ac1318144fc9c381af3a6d42d65f
Daksh Viradiya <dakshviradiya77@gmail.com>
2024-06-28 16:14:44 -0400

    Binary Search Tree. What it is and Why to use.

---
+++ 23. Binary Search Tree/BST.py
@@ -0,0 +1,8 @@
+#! What is BST?
+
+# Left Subtree the value of node is less than or equal to its parent node value.
+# Right Subtree the value of the node is greater than its parent nodes value.
+
+# Why need BST?
+
+# Insertion and Deletion is Faster then Binary tree.
