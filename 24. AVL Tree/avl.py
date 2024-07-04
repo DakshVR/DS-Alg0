4743c5165c1700d614272bbe401e3d5004387b60
Daksh Viradiya <dakshviradiya77@gmail.com>
2024-07-04 11:57:29 -0400

    Starting AVL Tree.

---
+++ 24. AVL Tree/avl.py
@@ -0,0 +1,10 @@
+
+#* What is an Avl tree?
+#  AVL tree is a self-balancing Binary Search Tree (BST) where the difference between heights of the left and right subtrees cannot be more than one for all the nodes.
+
+# All properties of BST applies to AVL.
+# Additional property, the tree must be balanced, making BST balanced.
+
+# If at any time heights of left and right subtree differ by more than one, then rebalancing is done to restore AVL propery, this process is called ROTATION.
+
+#* Why we need Avl tree?
