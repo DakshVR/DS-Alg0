7301ea51a28abb456a154d1e64bff024dba491ae
Daksh Viradiya <dakshviradiya77@gmail.com>
2024-07-05 19:46:41 -0400

    Worst case comparison btwn BST and AVL

---
+++ 24. AVL Tree/WorstCaseBSTandAVL.py
@@ -0,0 +1,13 @@
+
+# ! Time and Space Complexity of BST VS AVL
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# *                   #       BST           #           AVL         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Create Tree         #        O(1)         #           O(1)        #
+# Insert Node         #        O(N)         #         O(logN)       #
+# Traverse            #        O(N)         #           O(N)        #
+# Search node in      #        O(N)         #         O(logN)       #
+# Delete node in      #        O(N)         #         O(logN)       #
+# Delete Tree         #        O(1)         #           O(1)        #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
