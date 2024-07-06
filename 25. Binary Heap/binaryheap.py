ac7daf41b4614e70c0a94b8cc3ccf7f3073f05f4
Daksh Viradiya <dakshviradiya77@gmail.com>
2024-07-06 11:30:51 -0400

    Binary Heap. What is it?

---
+++ 25. Binary Heap/binaryheap.py
@@ -0,0 +1,6 @@
+
+#? What is a Binary Heap
+#* A Binary Heap is a Bunary Tree with the followinf properties.
+
+# --> A Binary heap is either Min Heap or Max Heap. In a Min Binary Heap, the key at the root must be minimum among the keys present in Binary Heap. The same property must be recursivey true for all the nodes in Binary Tree.
+# --> Its a oomplete tree (All levels are completely filled except possibly the last level and the least level has all the keys as lest as possible). This property of Binary Heap makes them suitable to be stored in an array.
