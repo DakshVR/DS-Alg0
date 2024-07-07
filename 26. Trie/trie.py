c4749fd67df87513e482d32d8e0190374e994808
Daksh Viradiya <dakshviradiya77@gmail.com>
2024-07-07 16:59:32 -0400

    Trie, What is it? Why we need it?

---
+++ 26. Trie/trie.py
@@ -0,0 +1,10 @@
+
+#? What is Trie and Why we need it
+#  A Trie is a tree-based data structure that organizes information in a hierarchy.
+# It is often used for strings. It stores characters at each node, making it easy for prefix matxhing.
+
+#* Properties
+# It is typically used to store or search steings in a spcae and time efficient way
+# Any node in trie can store non repetitive multiple characters
+# Every node stores link of the next character of the string
+# Every node keeps track of "end of string"
