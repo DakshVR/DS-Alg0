fec1a88934fe17451b2dc7ec0927fe99718de3a9
Daksh Viradiya <dakshviradiya77@gmail.com>
2024-07-09 10:58:54 -0400

    What is Sorting? and why we need?

---
+++ 28. Sort Algorithm/sort.py
@@ -0,0 +1,9 @@
+
+#? What is Sorting?
+#  Sorting refers to arranging data in a particular order: ascending or descending.
+# 1, 2, 3, 4, 5, 6, 7
+# 7, 6, 5, 4, 3, 2, 1
+
+#* Practical Use
+#  Microsoft Excel functionality to sort data
+#  Online product website: sort by price: low to high
