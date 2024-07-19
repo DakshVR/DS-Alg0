4783ab230b039b47506a9c14e57f0c0e3268a5f0
Daksh Viradiya <dakshviradiya77@gmail.com>
2024-07-19 13:35:53 -0400

    Graph Representation

--- 30. Graph Algorithm/graph.py
+++ 30. Graph Algorithm/graph.py
@@ -39,3 +39,36 @@
 #   |       |
 #   |       |----> Unweighted
 
+
+#!  Graph Representation
+
+#*  Adjacency Matrix: An Adjacency matrix is a square matrix or a 2D array. The elements of the matrxi indicate weather pairs of vertices are adjacent or not in the graph
+
+#                               #  A  #  B  #  C  #  D  #  E  #   
+#   A -- B                   A  #  0  #  1  #  1  #  1  #  0  # 
+#   | \   \                  B  #  1  #  0  #  0  #  0  #  1  #  
+#   |  \   E                 C  #  1  #  0  #  0  #  1  #  0  # 
+#   |   \ /                  D  #  1  #  0  #  1  #  0  #  1  # 
+#   C -- D                   E  #  0  #  1  #  0  #  1  #  0  # 
+
+
+#*  Adjacency List: An adjacency list is a collection of unordered list used to represent a graph. Each list describes the set of neighbours of a vertex in the graph.
+
+#   A -- B                A ---->  B ---->  C ---->  D
+#   | \   \               B ---->  A ---->  E
+#   |  \   E              C ---->  A ---->  D
+#   |   \ /               D ---->  A ---->  C ---->  E
+#   C -- D                E ---->  B ---->  D
+
+#! If a graph is complete or almost complete we should use Adjacency Matrix
+#! If the number of edges are few then we should use Adjacency List 
+
+#*  In Python it is useful to use Adjacency List. We can use DIctinoary.
+#*  Keys will be the Verticies(Nodes), 
+#*  Values will be the Edges.
+
+#   A -- B              { A : [B, C, D],
+#   | \   \               B : [A, E],
+#   |  \   E              C : [A, D],
+#   |   \ /               D : [A, C, E],
+#   C -- D                E : [B, D] }
