1d18e8bb9dd64c361e184ad590e70fc65212a7fe
Daksh Viradiya <dakshviradiya77@gmail.com>
2024-07-19 13:06:22 -0400

    Graph What is it? Why we need? Terminologies.

---
+++ 30. Graph Algorithm/graph.py
@@ -0,0 +1,18 @@
+
+#! What is a Graph.
+#  Graph consists of a finite set of Vertices(or Nodes) and set of Edges which connect a pair of nodes.
+
+#! Why we need Graph.
+#  It used to represent networks: path in city, circuit.
+
+
+#? Terminology
+#  Vertices(Vertex): Vertices are the nodes of the graph.
+#  Edge: The edge is a line that connects pairs of vertices.
+#  Unweighted graph: A graph which does not have a weight associated with any  edge
+#  Weighted graph: A graph which has a weight asscoiated with any edge.
+#  Undirected graph: In case the edges of the graph do not have a direction associated with them.  
+#  Directed graph: If the edges in a graph have a direction assocated with them
+#  Cyclic graph: A graph which has at least one loop
+#  Acyclic graph: A graph with no loop
+#  Tree: It is a special case of directed acyclic graph
