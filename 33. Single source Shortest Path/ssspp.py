3429f7ee1a69f8a6831483770b68dc5d98776d7c
Daksh Viradiya <dakshviradiya77@gmail.com>
2024-07-23 15:10:35 -0400

    Dijkstras Algorithm for shortest path

--- 33. Single source Shortest Path/ssspp.py
+++ 33. Single source Shortest Path/ssspp.py
@@ -1,4 +1,6 @@
 
+#! Single Source Shortest Path Problem
+#  A single source problem is about finding a path between a given vertex(source) to all the other vertices in a graph such that distance between them(source and destination) is minimum
 
 class Graph:
     def __init__(self, gdict=None):
@@ -6,6 +8,23 @@ class Graph:
             gdict = {}
         self.gdict = gdict
 
+
+    #? Problem:
+    #  - Five offices in different cities.
+    #  - Travel costs between cites are known.
+    #  - Find the cheapest way from head office to branches in different cities.
+
+    #* BFS
+    #  Algorithm
+    #  enqueue any starting vertex
+    #          while the queue is not empty:
+    #                p = dequeue()
+    #                if p is unvisited:
+    #                    mark it visited
+    #                    enqueue all adjacent unvisited vertices
+    #                    update parent of adjacent vertices to currentvertex 
+    #!  Doesn't work with weighted graph
+        
     def bfs(self, start, end):
         queue = []
         queue.append([start])
@@ -19,23 +38,7 @@ class Graph:
                 new_path.append(adjacent)
                 queue.append(new_path)
 
-#! Single Source Shortest Path Problem
-#  A single source problem is about finding a path between a given vertex(source) to all the other vertices in a graph such that distance between them(source and destination) is minimum
 
-#? Problem:
-#  - Five offices in different cities.
-#  - Travel costs between cites are known.
-#  - Find the cheapest way from head office to branches in different cities.
-
-#* BFS
-#  Algorithm
-#  enqueue any starting vertex
-#          while the queue is not empty:
-#                p = dequeue()
-#                if p is unvisited:
-#                    mark it visited
-#                    enqueue all adjacent unvisited vertices
-#                    update parent of adjacent vertices to currentvertex 
 
 customDict = {
     "a": ["b", "c"],
---
+++ 34. Graph Algo - Dijkstra's Algo/dijkstras.py
@@ -0,0 +1,97 @@
+
+
+import heapq
+
+#! class for edge
+class Edge:
+    def __init__(self, weight, start_vertex, target_vertex):
+        self.weight = weight
+        self.start_vertex = start_vertex
+        self.target_vertex = target_vertex
+
+#! Class for Nodes
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.visited = False
+        #? previous node: the node where we came from
+        self.predecessor = None
+        self.neighbours = []
+        self.min_distance = float('inf')
+
+    def __lt__(self, other_node):
+        return self.min_distance < other_node.min_distance
+    
+    def addEdge(self, weight, destination_vertex):
+        edge = Edge(weight, self, destination_vertex)
+        self.neighbours.append(edge)
+
+# Dijkstra's Algorithm     
+class Dijkstra:
+    def __init__(self):
+        self.heap = []
+
+    def calculate(self, start_vertex):
+        start_vertex.min_distance = 0
+        heapq.heappush(self.heap, start_vertex)
+        while self.heap:
+            # pop element with lowest distance
+            actual_vertex = heapq.heappop(self.heap)
+            if actual_vertex.visited:
+                continue
+            # consider neighbours
+            for edge in actual_vertex.neighbours:
+                start = edge.start_vertex
+                target = edge.target_vertex
+                new_distance = start.min_distance + edge.weight
+                if new_distance < target.min_distance:
+                    target.min_distance = new_distance
+                    target.predecessor = start
+                    # Update Heap
+                    heapq.heappush(self.heap, target)
+            actual_vertex.visited = True
+
+    def get_shortest_path(self, vertex):
+        print(f"Sortest Path to Vertex is: {vertex.min_distance}")
+        actual_vertex = vertex
+        while actual_vertex is not None:
+            print(actual_vertex.name, end =" ")
+            actual_vertex = actual_vertex.predecessor
+
+# Step 1 = Create Node
+nodeA = Node("A")
+nodeB = Node("B")
+nodeC = Node("C")
+nodeD = Node("D")
+nodeE = Node("E")
+nodeF = Node("F")
+nodeG = Node("G")
+nodeH = Node("H")
+
+# Step 2: Add Edges
+nodeA.addEdge(6, nodeB)
+nodeA.addEdge(10, nodeC)
+nodeA.addEdge(9, nodeD)
+
+nodeB.addEdge(5, nodeD)
+nodeB.addEdge(16, nodeE)
+nodeB.addEdge(13, nodeF)
+
+nodeC.addEdge(6, nodeD)
+nodeC.addEdge(5, nodeH)
+nodeC.addEdge(21, nodeG)
+
+nodeD.addEdge(8, nodeF)
+nodeD.addEdge(7, nodeH)
+
+nodeE.addEdge(10, nodeG)
+
+nodeF.addEdge(4, nodeE)
+nodeF.addEdge(12, nodeG)
+
+nodeH.addEdge(2, nodeF)
+nodeH.addEdge(14, nodeG)
+
+algorithm = Dijkstra()
+algorithm.calculate(nodeA)
+algorithm.get_shortest_path(nodeG)
