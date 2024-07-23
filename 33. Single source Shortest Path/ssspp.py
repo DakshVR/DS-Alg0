e714739fe46d4b0787c9ba3137b9852f90ce9b28
Daksh Viradiya <dakshviradiya77@gmail.com>
2024-07-23 13:48:33 -0400

    BFS for single sorce shortest path

---
+++ 33. Single source Shortest Path/ssspp.py
@@ -0,0 +1,52 @@
+
+
+class Graph:
+    def __init__(self, gdict=None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+
+    def bfs(self, start, end):
+        queue = []
+        queue.append([start])
+        while queue:
+            path = queue.pop(0)
+            node = path[-1]
+            if node == end:
+                return path
+            for adjacent in self.gdict.get(node, []):
+                new_path = list(path)
+                new_path.append(adjacent)
+                queue.append(new_path)
+
+#! Single Source Shortest Path Problem
+#  A single source problem is about finding a path between a given vertex(source) to all the other vertices in a graph such that distance between them(source and destination) is minimum
+
+#? Problem:
+#  - Five offices in different cities.
+#  - Travel costs between cites are known.
+#  - Find the cheapest way from head office to branches in different cities.
+
+#* BFS
+#  Algorithm
+#  enqueue any starting vertex
+#          while the queue is not empty:
+#                p = dequeue()
+#                if p is unvisited:
+#                    mark it visited
+#                    enqueue all adjacent unvisited vertices
+#                    update parent of adjacent vertices to currentvertex 
+
+customDict = {
+    "a": ["b", "c"],
+    "b": ["d", "g"],
+    "c": ["d", "e"],
+    "d": ["f"],
+    "e": ["f"],
+    "g": ["f"]
+}
+
+newGraph = Graph(customDict)
+print(newGraph.bfs("a", "f"))
+#* Dijkstra's Algorithm
+#* Bellman Ford
